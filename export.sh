409a3a4d42a33adeb36accda29865bf24ff585b3
ror <mister-al@ya.ru>
2012-06-08 11:35:38 +0600

    Убрано лишнее

--- export.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#mysqldump --user=root --password=fduecn --fields-escaped-by='\\' --fields-terminated-by='\t' --lines-terminated-by='\n' --default-character-set=utf8 "--tab=/tmp" sundmart jos_al_import
-
-rm /tmp/spree_export.csv
-mysql -uroot -pfduecn sundmart < export.sql
